9f621fd950585133aeba0b550f4d8c3822cdb343
Alexey <aovedernikov@yandex.ru>
2020-07-29 21:30:37 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,26 @@
+// const obj = {
+//     name: "John",
+//     age: 25,
+//     isMarried: false
+// };
+
+// console.log(obj.name);
+
+// const myObj = {
+//     0: "test",
+//     1: 11,
+//     2: 17
+// };
+// console.log(myObj[1]);
+
+//alert("Hello");
+// const result = confirm("Are you here?");
+
+// const answer = +prompt("Вам есть 18?", "18");
+// console.log(answer+5);
+
+const answers = [];
+answers[0] = prompt("Вопрос 1?", ""); console.log(answers[0]);
+answers[1] = prompt("Вопрос 2?", ""); console.log(answers[1]);
+answers[2] = prompt("Вопрос 3?", ""); console.log(answers[2]);
+
