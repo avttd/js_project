0321a1bebde2978a4e1961146ff80c29596479a2
Alexey <aovedernikov@yandex.ru>
2020-08-01 18:20:41 +0300

    hw 03

--- base/js/script.js
+++ base/js/script.js
@@ -79,6 +79,7 @@ for (let i = 1; i < 8; i++) {
 }
 
 // Функции
+// FUNCTION DECLARATION
 function showFirstMessage() {     //имя функции - глагол с припиской, что функция делает
     console.log("Hello World!");
 }
@@ -88,5 +89,63 @@ showFirstMessage();
 function showFirstMessage(text) {     //имя функции - глагол с припиской, что функция делает
     console.log(text);
 }
-
 showFirstMessage("Hello World!");
+
+// замыкание функции - это сама функция вместе со всеми внешними переменными, которые их доступны
+
+// function calc(a, b) {
+//     return(a + b);      //возврат значения функции
+// }
+
+// console.log(calc(4, 3));
+
+
+function ret() {
+    let num = 100;
+    return(num);
+}
+
+const anotherNum = ret();
+console.log(anotherNum);
+
+// FUNCTION EXPRESSION
+const logger = function() {
+    console.log("Hello!");
+};
+
+logger();
+
+//Стрелочная функция
+const calc = (a, b) => a + b;  // const calc = (a, b) => { return a + b };
+
+// МЕТОДЫ И СВОЙСТВА
+// Методы - это вспомогательные функции
+// Свойства - это вспомогательные значения
+
+const str = "teSt";
+console.log(str.length);        // это свойство
+
+const arr = [1, 2, 3];
+console.log(arr.length);        // это свойство
+
+console.log(str.toUpperCase());     //это метод
+console.log(str.toLowerCase());
+
+let fruit = "some fruits";
+// console.log(fruit.indexOf("fruit"));    // подстрока
+console.log(fruit.indexOf("s"));
+
+const logg = "Hello world";
+console.log(logg.slice(6, 11));         // взять кусок строки -> world
+console.log(logg.slice(-5, -1));        //с 5-го до 1-го символа справа 
+
+console.log(logg.substring(6, 11));     // аналог slice, но без -
+console.log(logg.substr(6, 2));         //  сколько символов берем
+
+const newNum = 12.4;
+console.log(Math.round(newNum));        //округление
+
+const test = "12.2px";
+console.log(parseInt(test));        //фишка 1
+console.log(parseFloat(test));      //фишка 2
+
